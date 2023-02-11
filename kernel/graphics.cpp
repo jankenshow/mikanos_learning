10ae7bd326d52cc343ede3353f81951957bf65bb
jankenshow <perchon001@gmail.com>
2023-02-11 16:38:53 +0900

    lint and format

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -10,15 +10,15 @@ uint8_t *PixelWriter::PixelAt(int x, int y)
 void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor &c)
 {
     auto p = PixelAt(x, y);
-    p[0] = c.r;
-    p[1] = c.g;
-    p[2] = c.b;
+    p[0]   = c.r;
+    p[1]   = c.g;
+    p[2]   = c.b;
 }
 
 void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor &c)
 {
     auto p = PixelAt(x, y);
-    p[0] = c.b;
-    p[1] = c.g;
-    p[2] = c.r;
+    p[0]   = c.b;
+    p[1]   = c.g;
+    p[2]   = c.r;
 }
--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -8,26 +8,25 @@ struct PixelColor
 };
 
 // フレームバッファを介して、ピクセル描画を行うベースクラス
-class PixelWriter
-{
-public:
+class PixelWriter {
+  public:
     PixelWriter(const FrameBufferConfig &config);
     virtual ~PixelWriter() = default;
-    virtual void Write(int x, int y, const PixelColor &c) = 0; // 純粋仮想関数 (オーバーライドされないとエラーになる)
+    virtual void Write(int x, int y,
+                       const PixelColor &c) = 0;    // 純粋仮想関数 (オーバーライドされないとエラーになる)
 
-protected:
+  protected:
     // 指定された座標のピクセルに関して、フレームバッファ上のアドレスを返す
     // 一つのピクセルあたり4バイトの大きさをもつ。
     uint8_t *PixelAt(int x, int y);
 
-private:
+  private:
     const FrameBufferConfig &config_;
 };
 
 // RGB形式のピクセルフォーマットに対する、ピクセル描画クラス
-class RGBResv8BitPerColorPixelWriter : public PixelWriter
-{
-public:
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
     using PixelWriter::PixelWriter;
 
     // override はつけなくても動くが、わかりやすくなるのでつけた方が良い
@@ -35,9 +34,8 @@ public:
 };
 
 // BGR形式のピクセルフォーマットに対する、ピクセル描画クラス
-class BGRResv8BitPerColorPixelWriter : public PixelWriter
-{
-public:
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
     using PixelWriter::PixelWriter;
 
     virtual void Write(int x, int y, const PixelColor &c) override;
