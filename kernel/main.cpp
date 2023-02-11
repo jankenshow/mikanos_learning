65062868f21ca824add14ccb0ddaf0d0155ca6ee
jankenshow <perchon001@gmail.com>
2023-02-11 16:38:34 +0900

    add printk function

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,16 +1,15 @@
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
 
+#include "console.hpp"
+#include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
-#include "font.hpp"
 
-// 配置new : メモリの確保は行わないが、指定したメモリ(buf)上にインスタンスを作成する
-void *operator new(size_t size, void *buf)
-{
-    return buf;
-}
+// 配置new :
+// メモリの確保は行わないが、指定したメモリ(buf)上にインスタンスを作成する
+void *operator new(size_t size, void *buf) { return buf; }
 
 // これを定義しないとエラーになるらしい (デストラクタが要求)
 void operator delete(void *obj) noexcept {}
@@ -23,55 +22,62 @@ extern "C" void __cxa_pure_virtual()
         __asm__("hlt");
 }
 
-// ピクセル描画クラスのメモリ確保 (配列によるメモリ確保は言語に元々備わっているため利用できる)
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+// ピクセル描画クラスのメモリ確保
+// (配列によるメモリ確保は言語に元々備わっているため利用できる)
+char         pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
+
+// コンソール用のメモリ確保
+char     console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...)
+{
+    va_list ap;
+    int     result;
+    char    s[1024];
+
+    va_start(ap, format);
+    result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->PutString(s);
+    return result;
+};
 
 // C++独自の参照渡し(参照型)で関数を定義しているが、C言語から呼び出す場合は
 // ポインタを指定すればOK. (System V AMD64 ABI(コンパイラ)の仕様で決まっている)
-// ABI = プログラム(関数など=呼出規約, Calling Convention)が動作するにあたり、必要なレジスタやメモリの使い方を定義したもの
+// ABI = プログラム(関数など=呼出規約, Calling
+// Convention)が動作するにあたり、必要なレジスタやメモリの使い方を定義したもの
 // コンパイラはこのABIに従って機械語を生成する。
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
 {
     // ピクセルの形式で描画クラスを変更する (ポリモフィズム)
-    switch (frame_buffer_config.pixel_format)
-    {
-    case kPixelRGBResv8BitPerColor:
-        // OSの機能が使えないため？C++のコンストラクタ呼び出しができない (通常のnewも同様)
-        // そのため配置newを利用
-        pixel_writer = new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
-        break;
-    case kPixelBGRResv8BitPerColor:
-        pixel_writer = new (pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
-        break;
+    switch (frame_buffer_config.pixel_format) {
+        case kPixelRGBResv8BitPerColor:
+            // OSの機能が使えないため？C++のコンストラクタ呼び出しができない
+            // (通常のnewも同様) そのため配置newを利用
+            pixel_writer = new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+        case kPixelBGRResv8BitPerColor:
+            pixel_writer = new (pixel_writer_buf) BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
     }
 
-    // ピクセルの描画
-    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x)
-    {
-        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y)
-        {
+    // ピクセル(背景)の描画
+    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
             pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
-    for (int x = 0; x < 200; ++x)
-    {
-        for (int y = 0; y < 100; ++y)
-        {
-            pixel_writer->Write(x, y, {0, 255, 0});
-        }
-    }
 
-    int i = 0;
-    for (char c = '!'; c <= '~'; ++c, ++i) {
-        WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-    }
-    WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
+    // コンソールクラスの初期化
+    console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-    // newlibを利用した書式整形
-    char buf[128];
-    sprintf(buf, "1 + 2 = %d", 1 + 2);
-    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+    // コンソールへの描画
+    for (int i = 0; i < 27; ++i) {
+        printk("printk: %d\n", i);
+    }
 
     while (1)
         // アセンブリを直接呼び出した方(インラインアセンブリ)が待機中のCPU使用率を節約できる
