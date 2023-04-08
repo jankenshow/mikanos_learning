57e322736c22241a5d676b1cedf8fe7679e80c28
jankenshow <perchon001@gmail.com>
2023-04-09 01:31:27 +0900

    add mouse cursor

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -22,6 +22,18 @@ extern "C" void __cxa_pure_virtual()
         __asm__("hlt");
 }
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+const int  kMouseCursorWidth                                             = 15;
+const int  kMouseCursorHeight                                            = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+    "@              ", "@@             ", "@.@            ", "@..@           ", "@...@          ", "@....@         ",
+    "@.....@        ", "@......@       ", "@.......@      ", "@........@     ", "@.........@    ", "@..........@   ",
+    "@...........@  ", "@............@ ", "@......@@@@@@@@", "@......@       ", "@....@@.@      ", "@...@ @.@      ",
+    "@..@   @.@     ", "@.@    @.@     ", "@@      @.@    ", "@       @.@    ", "         @.@   ", "         @@@   ",
+};
+
 // ピクセル描画クラスのメモリ確保
 // (配列によるメモリ確保は言語に元々備わっているため利用できる)
 char         pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
@@ -64,19 +76,30 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
             break;
     }
 
-    // ピクセル(背景)の描画
-    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-            pixel_writer->Write(x, y, {255, 255, 255});
-        }
-    }
+    const int kFrameWidth  = frame_buffer_config.horizontal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+    // 背景の描画
+    FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50}, kDesktopBGColor);
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50}, {1, 8, 17});
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth / 5, 50}, {80, 80, 80});
+    DrawRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30}, {160, 160, 160});
 
     // コンソールクラスの初期化
-    console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+    console = new (console_buf) Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
 
     // コンソールへの描画
-    for (int i = 0; i < 27; ++i) {
-        printk("printk: %d\n", i);
+    printk("Welcome to MikanOS!\n");
+
+    // マウスカーソルの描画
+    for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+        for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+            if (mouse_cursor_shape[dy][dx] == '@') {
+                pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+            } else if (mouse_cursor_shape[dy][dx] == '.') {
+                pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+            }
+        }
     }
 
     while (1)
